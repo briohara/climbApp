3b6b6d03049b72a69b602b59c10ed9674220d187
Brian O'Hara <c_bOhara@hyphensolutions.com>
2020-01-29 14:09:25 -0600

    Added jwts to requests. Updated routes model

--- server/api/users/userController.js
+++ server/api/users/userController.js
@@ -4,24 +4,26 @@ const signToken = require("../../auth/auth");
 
 //get user
 exports.getUser = function(req, res, next) {
-    console.log(JSON.stringify(req));
-    userModel.findById(req.body.user._id, (err, user) => {
-        if(err) return next(err);
-        console.log("Finding user by id...");
+  console.log(JSON.stringify(req));
+  userModel.findById(req.body.user._id, (err, user) => {
+    if (err) return next(err);
+    console.log("Finding user by id...");
 
-        //Successful!
-        res.json(user);
-    })
+    //Successful!
+    res.json(user);
+  });
 };
 
-exports.createUser = function (req, res, next) {
-    let newUser = new userModel(req.body);
-    console.log("New user: " + newUser);
+exports.createUser = function(req, res, next) {
+  let newUser = new userModel(req.body);
+  console.log("New user: " + newUser);
 
-    newUser.save((err, user) => {
-        if(err) {next(err);}
+  newUser.save((err, user) => {
+    if (err) {
+      next(err);
+    }
 
-        let token = signToken(user._id);
-        res.json({token: token});
-    });
+    let token = signToken(newUser._id);
+    res.json({ token: token });
+  });
 };
